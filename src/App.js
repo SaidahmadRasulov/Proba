9259aad1564405793dd84c04d2b0a0a653c5454f
SaidahmadRasulov <said.rasulov.2018@list.ru>
2023-04-17 14:02:52 +0500

    Finish

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1>App Section</h1>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,19 @@
+function Header() {
+    return (
+        <header>
+            <div className="container">
+                <nav className="navbar">
+                    <h1>Logo</h1>
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Service</li>
+                        <li>Contact</li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
